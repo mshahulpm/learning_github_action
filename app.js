e17139f86bf9229faef8a15a5de7ea28f081de6d
Mohammed Shahul P M <mshahulpm@gmail.com>
2022-09-29 16:59:19 +0530

    added some routes and data workflow edit

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require('express')
+const cors = require('cors')
+
+// data 
+const { books, users } = require('./db')
+
 const app = express()
+app.use(cors())
+
 
 
 app.get('/', (req, res) => {
@@ -7,6 +14,16 @@ app.get('/', (req, res) => {
         message: "welcome to node server"
     })
 })
+
+
+app.get('/users', (req, res) => {
+    res.json(users)
+})
+
+app.get('/books', (req, res) => {
+    res.json(books)
+})
+
 
 app.get('*', (req, res) => {
     res.status(404).json({
---
+++ db.js
@@ -0,0 +1,20 @@
+module.exports = {
+
+    users: [
+        { name: "shahul", age: 27, place: 'mlp' },
+        { name: "shameem", age: 27, place: 'mlp' },
+        { name: "arshad", age: 26, place: 'kgp' },
+        { name: "mohan", age: 26, place: 'mh' },
+        { name: "nazeem", age: 22, place: 'alp' },
+    ],
+
+    books: [
+        { title: 'Title 1', author: 'Author 1' },
+        { title: 'Title 2', author: 'Author 2' },
+        { title: 'Title 3', author: 'Author 3' },
+        { title: 'Title 4', author: 'Author 4' },
+        { title: 'Title 5', author: 'Author 5' },
+        { title: 'Title 6', author: 'Author 6' },
+    ]
+
+}
