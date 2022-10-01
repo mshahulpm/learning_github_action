c0fa84089d8c58db8f00baa5b444f16274706065
Mohammed Shahul P M <mshahulpm@gmail.com>
2022-10-01 21:06:02 +0530

    mongodb integrated

--- app.js
+++ app.js
@@ -1,27 +1,42 @@
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/mern_app',)
 
 // data 
 const { books, users } = require('./db')
+const User = require('./schema/user')
+const Books = require('./schema/books')
+
+
+// seeding data 
+setTimeout(async () => {
+    if (await Books.count() === 0) {
+        await Books.create(books)
+    }
+    if (await User.count() === 0) {
+        await User.create(users)
+    }
+}, 3000)
 
 const app = express()
 app.use(cors())
 
 
 
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
     res.json({
         message: "welcome to node server"
     })
 })
 
-
-app.get('/api/users', (req, res) => {
-    res.json(users)
+app.get('/users', async (req, res) => {
+    res.json(await User.find())
 })
 
-app.get('/api/books', (req, res) => {
-    res.json(books)
+app.get('/books', async (req, res) => {
+    res.json(await Books.find())
 })
 
 
---
+++ schema/books.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+
+
+const bookSchema = new mongoose.Schema({
+    title: String,
+    author: String
+})
+
+const Books = mongoose.model('Books', bookSchema)
+
+module.exports = Books
